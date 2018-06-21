b52e6404f668d8f260d0935e42f3e6ca9b8539cb
Alexey Popov <a.popov@zima.io>
2018-06-21 20:12:47 +0300

    fixed bug while subscription closed

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject alexeypopov/clj-nats-streaming-async "0.0.2"
+(defproject alexeypopov/clj-nats-streaming-async "0.0.3-SNAPSHOT"
   :description "an async client for NATS Streaming, wrapping java-nats-streaming client"
   :url "https://github.com/AlexeyPopov/clj-nats-async"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
   :dependencies [[alexeypopov/clj-nats-async "1.3.0"]
-                 [io.nats/java-nats-streaming "0.5.0"]
+                 [alexeypopov/java-nats-streaming "0.5.0-1-SNAPSHOT"]
                  [manifold "0.1.8"]]
 
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.9.0"]]}})
