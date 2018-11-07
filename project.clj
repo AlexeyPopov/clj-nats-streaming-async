0e73d38305b6e845037838c7ef068b95902d649c
Alexey Popov <a.popov@zima.io>
2018-11-07 19:29:53 +0300

    deps update

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject alexeypopov/clj-nats-streaming-async "0.0.8"
+(defproject alexeypopov/clj-nats-streaming-async "0.0.9-SNAPSHOT"
   :description "an async client for NATS Streaming, wrapping java-nats-streaming client"
   :url "https://github.com/AlexeyPopov/clj-nats-async"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
-  :dependencies [[io.nats/jnats "2.1.2"]
+  :dependencies [[io.nats/jnats "2.3.0"]
                  [io.nats/java-nats-streaming "2.1.2" :exclusions [io.nats/jnats]]
                  [manifold "0.1.8"]]
 
